f59fbcf41aa6b863bfdc7dd0c2580807ec779ade
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2025-04-06 20:26:49 +0200

    [AK] Konfiguracja frontendu: struktura folderów, instalacja zależności, usunięcie zbędnych plików

---
+++ gymlog.gui/src/app/App.tsx
@@ -0,0 +1,15 @@
+
+export default function App() {
+    return (
+        <>
+            <div className="flex justify-center items-center">
+                <div className="flex flex-col">
+                    <p className="flex flex-col">XD</p>
+                </div>
+                <div>
+                </div>
+                <p>Click on the Vite and React logos to learn more</p>
+            </div>
+        </>
+    );
+}
--- gymlog.gui/vite.config.js
+++ gymlog.gui/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 import plugin from '@vitejs/plugin-react';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), tailwindcss()],
     server: {
         port: 53345,
     }
