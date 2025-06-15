122d5b91ffed7e417b679fa6fbcdaec003dff21b
Konrad Koziel <konrad981204@gmail.com>
2025-06-15 15:08:51 +0200

    Add ModelDto and refactor controllers

---
+++ gymLog.API/Mappings/WorkoutDayProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using gymLog.API.Model.DTO.WorkoutDay;
+using gymLog.Model;
+
+namespace gymLog.API.Mappings
+{
+    public class WorkoutDayProfile : Profile
+    {
+        public WorkoutDayProfile()
+        {
+            CreateMap<WorkoutDay, GetExerciseDto>();
+            CreateMap<GetExerciseDto, WorkoutDay>();
+        }
+    }
+}
