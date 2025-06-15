1d56f797967bcd381b9b74b97af0b41499fe07a9
Konrad Koziel <konrad981204@gmail.com>
2025-06-15 11:06:22 +0200

    Add error handling middleware

---
+++ gymLog.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using gymLog.API.Exceptions;
+using gymLog.API.Services.interfaces;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace gymLog.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogService _log;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogService log)
+        {
+            _next = next;
+            _log = log;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _log.LogError(ex, "Unhandled exception");
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = ex switch
+                {
+                    NotFoundException => StatusCodes.Status404NotFound,
+                    ValidationException => StatusCodes.Status400BadRequest,
+                    UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                var errorResponse = new
+                {
+                    error = ex.Message,
+                    status = context.Response.StatusCode
+                };
+
+                await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
