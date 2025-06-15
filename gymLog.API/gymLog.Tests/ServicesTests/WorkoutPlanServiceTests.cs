cd9ebf727092439fe398fbfa07984c724d79cab3
Sebastian <mrseb10@gmail.com>
2025-06-15 21:58:01 +0200

    Created WorkoutPlanServiceTests template

---
+++ gymLog.API/gymLog.Tests/ServicesTests/WorkoutPlanServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using gymLog.API.Services;
+using gymLog.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace gymLog.Tests.ServicesTests;
+
+public class WorkoutPlanServiceTests {
+    WorkoutPlanService _workoutPlanService;
+
+    public WorkoutPlanServiceTests() {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        AppDbContext dbContext = new AppDbContext(options);
+        _workoutPlanService = new WorkoutPlanService(dbContext);
+        
+        //
+    }
+}
